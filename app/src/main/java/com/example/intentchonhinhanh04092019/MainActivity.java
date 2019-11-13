fae4fd23a63c18684a2bb8f0c9e2be5a2bca8236
phatdroid94 <phatdroid94@gmail.com>
2019-11-13 20:58:34 +0700

    add time

--- app/src/main/java/com/example/intentchonhinhanh04092019/MainActivity.java
+++ app/src/main/java/com/example/intentchonhinhanh04092019/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.intentchonhinhanh04092019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton imgPlay;
+    ImageView imgHinhGoc,imgHinhChon;
+    TextView txtThoiGian;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imgPlay = findViewById(R.id.imagebuttonPlay);
+        imgHinhGoc = findViewById(R.id.imageviewHinhGoc);
+        imgHinhChon = findViewById(R.id.imageviewHinhChon);
+        txtThoiGian = findViewById(R.id.textviewThoiGian);
     }
 }
